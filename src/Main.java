a22f8e311bdc7bdd352884e9780d1de2302ca121
MishkaMO <mishkamo@yandex.ru>
2022-11-04 16:26:42 +0300

    Увеличил переменную на 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
- var friend = 19;
+        var friend = 19;
 System.out.println(friend);
+friend = friend +2;
+System.out.println(friend);
+
+
     }
 }
 
