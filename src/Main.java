8180c62ba30bfffcde5caf642f560385e6ec69b5
MishkaMO <mishkamo@yandex.ru>
2022-11-04 17:29:26 +0300

    *10

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         var frog = 3.5;
 System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
 
 
 
